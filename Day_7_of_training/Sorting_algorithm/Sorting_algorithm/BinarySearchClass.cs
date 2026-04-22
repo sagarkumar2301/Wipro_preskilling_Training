d4c55fc41010c47db37b4793a5abbfeec090d37e
Sagar kumar <109957797+sagarkumar2301@users.noreply.github.com>
2026-04-22 14:06:25 +0530

    Initial commit

---
+++ Day_7_of_training/Sorting_algorithm/Sorting_algorithm/BinarySearchClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_algorithm
+{
+    class BinarySearchClass
+    {
+        public int BinarySearch(int[] arr, int key)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == key)
+                    return mid;
+                else if (arr[mid] < key)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Day_7_of_training/Sorting_algorithm/Sorting_algorithm/Program.cs
@@ -0,0 +1,46 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Sorting_algorithm;
+using System;
+
+class LinearSearchClass
+{
+    public static int LinearSearch(int[] arr, int key)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == key)
+                return i;
+        }
+        return -1;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 10, 20, 30, 40, 50 };
+
+        Console.WriteLine("Enter element to search:");
+        int key = Convert.ToInt32(Console.ReadLine());
+
+        
+        int linearResult = LinearSearchClass.LinearSearch(arr, key);
+
+        if (linearResult != -1)
+            Console.WriteLine("Linear Search: Element found at index " + linearResult);
+        else
+            Console.WriteLine("Linear Search: Element not found");
+
+        
+        BinarySearchClass bs = new BinarySearchClass();
+        int binaryResult = bs.BinarySearch(arr, key);
+
+        if (binaryResult != -1)
+            Console.WriteLine("Binary Search: Element found at index " + binaryResult);
+        else
+            Console.WriteLine("Binary Search: Element not found");
+    }
+}
+
