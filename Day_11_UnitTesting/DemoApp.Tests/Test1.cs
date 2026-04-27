b24505ea4a06d6751e5e40a7eb97c5a6a06eb375
Sagar kumar <109957797+sagarkumar2301@users.noreply.github.com>
2026-04-27 14:31:19 +0530

    Added Day 9, Day 10 and Day 11 .NET Program using CLI and MS Visual Studio

---
+++ Day_11_UnitTesting/DemoApp.Tests/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoApp.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        //Arrange
+        var calc = new Calcualtor();
+
+        // Act
+        var result = calc.Multiply(2, 3);
+
+        //Assert
+        Assert.AreEqual(6, result);
+    }
+}
---
+++ Day_11_Unit_Testing2/DemoApp2/DemoApp2.Test/Test1.cs
@@ -0,0 +1,19 @@
+﻿namespace DemoApp2.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var calculator = new Calcualtor();
+
+            //Act
+            var result = calculator.Multiply(2, 3);
+
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
