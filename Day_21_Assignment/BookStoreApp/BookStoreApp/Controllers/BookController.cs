54bb58ac95bd640203d82e21893653ba6f801e75
Sagar kumar <109957797+sagarkumar2301@users.noreply.github.com>
2026-05-14 23:47:05 +0530

    Added Day_21_Assignment

---
+++ Day_21_Assignment/BookStoreApp/BookStoreApp/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookStoreApp.DAL;
+using BookStoreApp.Models;
+
+namespace BookStoreApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookDAL dal;
+
+        public BookController(IConfiguration configuration)
+        {
+            dal = new BookDAL(configuration);
+        }
+
+        public IActionResult Index()
+        {
+            var books = dal.GetAllBooks();
+
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            dal.AddBook(book);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            dal.DeleteBook(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Day_21_Assignment/BookStoreApp/BookStoreApp/DAL/BookDAL.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using BookStoreApp.Models;
+
+namespace BookStoreApp.DAL
+{
+    public class BookDAL
+    {
+        private readonly string connectionString;
+
+        public BookDAL(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public List<Book> GetAllBooks()
+        {
+            List<Book> books = new List<Book>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Books";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Book book = new Book()
+                    {
+                        BookId = Convert.ToInt32(reader["BookId"]),
+                        Title = reader["Title"].ToString(),
+                        Author = reader["Author"].ToString(),
+                        Price = Convert.ToDecimal(reader["Price"]),
+                        Quantity = Convert.ToInt32(reader["Quantity"])
+                    };
+
+                    books.Add(book);
+                }
+            }
+
+            return books;
+        }
+        public void AddBook(Book book)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_AddBook", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Title", book.Title);
+                cmd.Parameters.AddWithValue("@Author", book.Author);
+                cmd.Parameters.AddWithValue("@Price", book.Price);
+                cmd.Parameters.AddWithValue("@Quantity", book.Quantity);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void UpdateBook(Book book)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_UpdateBook", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@BookId", book.BookId);
+                cmd.Parameters.AddWithValue("@Title", book.Title);
+                cmd.Parameters.AddWithValue("@Author", book.Author);
+                cmd.Parameters.AddWithValue("@Price", book.Price);
+                cmd.Parameters.AddWithValue("@Quantity", book.Quantity);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void DeleteBook(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_DeleteBook", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@BookId", id);
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public DataSet GetBooksDataSet()
+        {
+            DataSet ds = new DataSet();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Books";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+
+                adapter.Fill(ds, "Books");
+            }
+
+            return ds;
+        }
+    }
+}
