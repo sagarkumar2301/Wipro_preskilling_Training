5b2c761668fef9f49b5908e9c74e5940820bf390
Sagar kumar <109957797+sagarkumar2301@users.noreply.github.com>
2026-06-04 14:07:30 +0530

    Fix Template string

--- CloudAssignment/backend/server.js
+++ CloudAssignment/backend/server.js
@@ -18,5 +18,5 @@ app.get("/api/message", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+    console.log(`Server running on port ${PORT}`);
 });
