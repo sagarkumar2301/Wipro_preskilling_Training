62bf406c2ac9b5f5da47f21f0e61150c44ae0b7c
Sagar kumar <109957797+sagarkumar2301@users.noreply.github.com>
2026-06-04 13:39:59 +0530

    Fix Azure port

--- CloudAssignment/backend/server.js
+++ CloudAssignment/backend/server.js
@@ -5,12 +5,18 @@ const app = express();
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Backend is running on Azure");
+});
+
 app.get("/api/message", (req, res) => {
     res.json({
         message: "Hello from Azure"
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
 });
